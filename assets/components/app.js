ad49dbd500bad5a3bc1b71423707659a4b77ded3
KalininLev <kiilshoe@gmail.com>
2023-08-25 00:46:42 +0500

    small changes

--- assets/components/app.js
+++ assets/components/app.js
@@ -1,19 +1,22 @@
 const startBtn = document.querySelector('.first_page__start-btn');
 const chooseDiff = document.getElementsByName('difficult');
-const pageTitle = document.querySelector('.first_page__title');
-const pageDiff = document.querySelector('.first_page__difficulties-options');
+const startPage = document.querySelector('.first_page__nav');
+const gamePage = document.querySelector('.game_page');
+const restartBtn = document.querySelector('.game_page__top-restart-btn');
 
-
-startBtn.addEventListener('click', ()=>{
-    for (let i = 0; i < chooseDiff.length; i++){
-        if(chooseDiff[i].checked == true){
+startBtn.addEventListener('click', () => {
+    for (let i = 0; i < chooseDiff.length; i++) {
+        if (chooseDiff[i].checked === true) {
             console.log(chooseDiff[i]);
-            window.currentDiff = {difficult: chooseDiff[i].id}
+            window.currentDiff = { difficult: chooseDiff[i].id };
 
-            pageTitle.innerHTML = `Выбрана сложность: ${window.currentDiff.difficult}`
-            
-            pageDiff.classList.add('hidden')
-            startBtn.classList.add('hidden')
+            startPage.classList.add('hidden');
+            gamePage.classList.remove('hidden');
         }
     }
-})
+});
+
+restartBtn.addEventListener('click', () => {
+    startPage.classList.remove('hidden');
+    gamePage.classList.add('hidden');
+});
